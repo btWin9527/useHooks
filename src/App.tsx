86b8e70b827ad43db0b894ded61af0ed85b4924e
guojiguang <guojiguang@kaikeba.com>
2021-11-05 17:47:42 +0800

    chore: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UseMemoDemo from './components/UseMemoDemo';
+
+function App() {
+
+    return (
+        <div className="App">
+            <UseMemoDemo/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/UseMemoDemo/index.tsx
@@ -0,0 +1,43 @@
+import {useMemo, useState} from 'react';
+
+const UseMemoDemo = () => {
+    const [count, setCount] = useState(0);
+    const [wordIndex, setWordIndex] = useState(0);
+
+    const words = ['hey', 'this', 'is', 'cool'];
+    const word = words[wordIndex];
+
+    const computeLetterCount = (word: string) => {
+        let i = 0;
+        while (i < 1000000000) i++;
+        return word.length;
+    };
+
+    const letterCount = useMemo(() => computeLetterCount(word), [word]);
+
+    return (
+        <div style={{padding: '15px'}}>
+            <h2>Compute number of letters (slow 🐌)</h2>
+
+            <p>
+                "{word}" has {letterCount} letters
+            </p>
+
+            <button
+                onClick={() => {
+                    const next = wordIndex + 1 === words.length ? 0 : wordIndex + 1;
+                    setWordIndex(next);
+                }}
+            >
+                Next word
+            </button>
+            <br/>
+            <br/>
+            <h2>Increment a counter (fast ⚡️)</h2>
+            <p>Counter: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
+}
+
+export default UseMemoDemo;
